a6fc3f87ea4b948263389ea14e6b17013e28bbf2
minkee009 <minkee5004@gmail.com>
2024-07-18 10:52:19 +0900

    24-07-18
    - EBO코드 추가

--- src/context.cpp
+++ src/context.cpp
@@ -1,55 +1,63 @@
 #include "context.h"
 
 ContextUPtr Context::Create() {
-    auto context = ContextUPtr(new Context());
-    if (!context->Init())
-        return nullptr;
-    return std::move(context);
+	auto context = ContextUPtr(new Context());
+	if (!context->Init())
+		return nullptr;
+	return std::move(context);
 }
 
 bool Context::Init() {
-    float vertices[] = {
-        // first triangle
-        0.5f, 0.5f, 0.0f,  // top right
-        0.5f, -0.5f, 0.0f, // bottom right
-        -0.5f, 0.5f, 0.0f, // top left
-        // second triangle
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-        -0.5f, 0.5f, 0.0f   // top left
-    };
 
-    //VAO 생성
-    glGenVertexArrays(1, &m_vertexArrayObject);
-    glBindVertexArray(m_vertexArrayObject); //현재 상태에 VAO연결
+	float vertices[] = {
+		 0.5f,  0.5f, 0.0f, // top right
+		 0.5f, -0.5f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f, // bottom left
+		-0.5f,  0.5f, 0.0f, // top left
+	};
 
-    //정점버퍼 생성
-    glGenBuffers(1, &m_vertexBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 18, vertices, GL_STATIC_DRAW);
+	uint32_t indices[] = { // note that we start from 0!
+		0, 1, 3, // first triangle
+		1, 2, 3, // second triangle
+	};
 
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+	//VAO 생성
+	glGenVertexArrays(1, &m_vertexArrayObject);
+	glBindVertexArray(m_vertexArrayObject); //현재 상태에 VAO연결
 
-    ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
-    ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
-    if (!vertShader || !fragShader)
-        return false;
-    SPDLOG_INFO("vertex shader id: {}", vertShader->Get());
-    SPDLOG_INFO("fragment shader id: {}", fragShader->Get());
-    m_program = Program::Create({fragShader, vertShader});
-    if (!m_program)
-        return false;
-    SPDLOG_INFO("program id: {}", m_program->Get());
+	//정점버퍼 생성
+	glGenBuffers(1, &m_vertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 12, vertices, GL_STATIC_DRAW);
 
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
 
-    return true;
+	glGenBuffers(1, &m_indexBuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_indexBuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint32_t) * 6,
+		indices, GL_STATIC_DRAW);
+
+	ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
+	ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
+	if (!vertShader || !fragShader)
+		return false;
+	SPDLOG_INFO("vertex shader id: {}", vertShader->Get());
+	SPDLOG_INFO("fragment shader id: {}", fragShader->Get());
+	m_program = Program::Create({ fragShader, vertShader });
+	if (!m_program)
+		return false;
+	SPDLOG_INFO("program id: {}", m_program->Get());
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	return true;
 }
 
-	void Context::Render() {
-    glClear(GL_COLOR_BUFFER_BIT);
- 
-    glUseProgram(m_program->Get());
-    glDrawArrays(GL_TRIANGLES, 0, 6);
+void Context::Render() {
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(m_program->Get());
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	//glDrawArrays(GL_TRIANGLES, 0, 6);
 }
